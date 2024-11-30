3982da1bb4f8f8640deeca19eea73906bffef45c
kkazdal <37306406+kkazdal@users.noreply.github.com>
2024-11-30 11:45:21 +0300

    feat: Dashboard için arayüz tamamlandı. Servisler yazılacak

---
+++ Frontend/CarBookWebUI/ViewComponents/DashboardComponents/_AdminDashboardChart1ComponentPartial.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebUI.ViewComponents.DashboardComponents;
+
+public class _AdminDashboardChart1ComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ Frontend/CarBookWebUI/ViewComponents/DashboardComponents/_AdminDashboardChart3ComponentPartial.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebUI.ViewComponents.DashboardComponents;
+
+public class _AdminDashboardChart3ComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+
+}
